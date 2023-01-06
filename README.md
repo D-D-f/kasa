85a2e1eebec5aebf711d92f3da8e657b13f0f424
D-D-F <defreitas-david@outlook.fr>
2023-01-06 12:18:17 +0100

    modif responsive de la page description

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projet 8 Création d'un site immobilier : Kasa
+
+Pour lancer le projet : utiliser la commande `pnpm install` dans le terminal
+
+## Étapes de création :
+
+- Je découpe la maquette en composant et sous composant
+- Récupération des logos et images sur Figma
+-
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,7 +13,13 @@ const About = () => {
     <div className="About">
       <div className={style.images}></div>
       <div className={style.collapses}>
-        <Collapse name="Fiabilité" description={"test"} taille="100%" />
+        <Collapse
+          name="Fiabilité"
+          description={
+            "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+          }
+          taille="100%"
+        />
         <Collapse
           name="Respect"
           description={
@@ -21,8 +27,20 @@ const About = () => {
           }
           taille="100%"
         />
-        <Collapse name="Service" description={"test"} taille="100%" />
-        <Collapse name="Responsabilité" description={"test"} taille="100%" />
+        <Collapse
+          name="Service"
+          description={
+            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+          }
+          taille="100%"
+        />
+        <Collapse
+          name="Responsabilité"
+          description={
+            "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          }
+          taille="100%"
+        />
       </div>
     </div>
   );
